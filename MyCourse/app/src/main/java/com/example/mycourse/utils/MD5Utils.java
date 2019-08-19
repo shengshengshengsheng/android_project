6f0d21408392a5f9ffdd4b9c22188ed678936c08
xuqiangsheng <xuqiangsheng@rfchina.com>
2019-08-19 17:50:44 +0800

    注册页面的业务逻辑，还没实现保存用户账号密码

---
+++ MyCourse/app/src/main/java/com/example/mycourse/utils/MD5Utils.java
@@ -0,0 +1,34 @@
+package com.example.mycourse.utils;
+
+import android.text.TextUtils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Utils {
+    /**
+     * MD5加密算法，对用户密码进行加密操作
+     * */
+    public static String md5(String string) {
+        if (TextUtils.isEmpty(string)) {
+            return "";
+        }
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest(string.getBytes());
+            String result = "";
+            for (byte b : bytes) {
+                String temp = Integer.toHexString(b & 0xff);
+                if (temp.length() == 1) {
+                    temp = "0" + temp;
+                }
+                result += temp;
+            }
+            return result;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
